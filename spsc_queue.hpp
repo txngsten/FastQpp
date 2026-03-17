628f48c8e72b80cf665cd2fd7a3ef1577d60366d
wutt0019 <wutt0019@flinders.edu.au>
2026-03-17 18:52:37 +1030

    Added move assignment and constructors and deleted copy assignment and constructors, made some minor tweaks as well

--- spsc_queue.hpp
+++ spsc_queue.hpp
@@ -2,17 +2,46 @@
 #define SPSC_QUEUE_H
 
 #include <memory>
+#include <type_traits>
+#include <utility>
 
 namespace fastq {
-    template <typename T, typename Allocator = std::allocator<T>>
+    template <typename T>
+    concept TriviallyCopyable = std::is_trivially_copyable_v<T>;
+
+    template <TriviallyCopyable T, typename Allocator = std::allocator<T>>
     class SPSC {
       public:
-        explicit SPSC(std::size_t capacity) : capacity_(capacity), size_(0), alloc_() {
-            buffer_ = std::allocator_traits<Allocator>::allocate(alloc_, capacity_);
+        explicit SPSC(std::size_t capacity)
+            : capacity_(capacity), size_(0), alloc_(),
+              buffer_(std::allocator_traits<Allocator>::allocate(alloc_, capacity_)) {}
+
+        SPSC(const SPSC&) = delete;
+
+        SPSC& operator=(const SPSC&) = delete;
+
+        SPSC(SPSC&& other)
+            : alloc_(std::move(other.alloc_)), buffer_(std::exchange(other.buffer_, nullptr)),
+              capacity_(std::exchange(other.capacity_, 0)), size_(std::exchange(other.size_, 0)) {}
+
+        SPSC& operator=(SPSC&& other) noexcept {
+            if (this != &other) {
+                if (buffer_) {
+                    std::allocator_traits<Allocator>::deallocate(alloc_, buffer_, capacity_);
+                }
+
+                alloc_ = std::move(other.alloc_);
+                buffer_ = std::exchange(other.buffer_, nullptr);
+                capacity_ = std::exchange(other.capacity_, 0);
+                size_ = std::exchange(other.size_, 0);
+            }
+            return *this;
         }
 
         ~SPSC() {
-            std::allocator_traits<Allocator>::deallocate(alloc_, buffer_, capacity_);
+            if (buffer_) {
+                std::allocator_traits<Allocator>::deallocate(alloc_, buffer_, capacity_);
+            }
         }
 
         bool push() {
@@ -32,7 +61,7 @@ namespace fastq {
         }
 
         std::size_t capacity() const {
-            return capacity_ - size_;
+            return capacity_;
         }
 
         bool empty() const {
